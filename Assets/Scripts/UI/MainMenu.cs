729da6237b00523bc0df3e3a5f729e0b221769aa
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-10-11 18:52:07 +0200

    added main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+  public class MainMenu : MonoBehaviour
+  {
+    // called by Unity UI
+    // ReSharper disable once UnusedMember.Global
+    public void Play()
+    {
+      SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+  }
+}
