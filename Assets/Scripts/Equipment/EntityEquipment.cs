2a2446dd10c080b211f0adbd0c801e538d5a54bc
CptAs <arpek2121@gmail.com>
2021-11-16 19:03:36 +0100

    Added Inventory UI and changed Display

--- Assets/Scripts/Equipment/EntityEquipment.cs
+++ Assets/Scripts/Equipment/EntityEquipment.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Equipment
@@ -26,14 +23,22 @@ namespace Equipment
         public static bool isItemDescriptionEnabled;
         private static bool _isItemConsumable;
 
+        public GameObject weaponSlot;
+        public GameObject helmetSlot;
+        public GameObject breastplateSlot;
+        public GameObject leggingsSlot;
+        public GameObject bootsSlot;
+        public GameObject necklaceSlot;
+        public GameObject ringSlot;
+        public GameObject glovesSlot;
+
         public Weapon weapon;
         public Helmet helmet;
         public Breastplate breastplate;
         public Leggings leggings;
         public Boots boots;
         public Necklace necklace;
-        public Ring leftRing;
-        public Ring rightRing;
+        public Ring ring;
         public Gloves gloves;
         public List<Item> backpack;
 
@@ -70,9 +75,89 @@ namespace Equipment
                 {
                     _slots[i].GetComponent<Image>().sprite = backpack[i].icon;
                     var x = i;
-                    _slots[i].GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(x));
+                    _slots[i].GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(backpack[x]));
+                    
                 }
-
+                if (!(weapon is null))
+                {
+                    weaponSlot.GetComponent<Image>().sprite = weapon.icon;
+                    weaponSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(weapon));
+                    weaponSlot.SetActive(true);
+                }
+                else
+                {
+                    weaponSlot.SetActive(false);
+                }
+                if (!(helmet is null))
+                {
+                    helmetSlot.GetComponent<Image>().sprite = helmet.icon;
+                    helmetSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(helmet));
+                    helmetSlot.SetActive(true);
+                }
+                else
+                {
+                    helmetSlot.SetActive(false);
+                }
+                if (!(breastplate is null))
+                {
+                    breastplateSlot.GetComponent<Image>().sprite = breastplate.icon;
+                    breastplateSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(breastplate));
+                    breastplateSlot.SetActive(true);
+                }
+                else
+                {
+                    breastplateSlot.SetActive(false);
+                }
+                if (!(leggings is null))
+                {
+                    leggingsSlot.GetComponent<Image>().sprite = leggings.icon;
+                    leggingsSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(leggings));
+                    leggingsSlot.SetActive(true);
+                }
+                else
+                {
+                    leggingsSlot.SetActive(false);
+                }
+                if (!(boots is null))
+                {
+                    bootsSlot.GetComponent<Image>().sprite = boots.icon;
+                    bootsSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(boots));
+                    bootsSlot.SetActive(true);
+                }
+                else
+                {
+                    bootsSlot.SetActive(false);
+                }
+                if (!(necklace is null))
+                {
+                    necklaceSlot.GetComponent<Image>().sprite = necklace.icon;
+                    necklaceSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(necklace));
+                    necklaceSlot.SetActive(true);
+                }
+                else
+                {
+                    necklaceSlot.SetActive(false);
+                }
+                if (!(ring is null))
+                {
+                    ringSlot.GetComponent<Image>().sprite = ring.icon;
+                    ringSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(ring));
+                    ringSlot.SetActive(true);
+                }
+                else
+                {
+                    ringSlot.SetActive(false);
+                }
+                if (!(gloves is null))
+                {
+                    glovesSlot.GetComponent<Image>().sprite = gloves.icon;
+                    glovesSlot.GetComponent<Button>().onClick.AddListener(()=>OnItemClicked(gloves));
+                    glovesSlot.SetActive(true);
+                }
+                else
+                {
+                    glovesSlot.SetActive(false);
+                }
                 iconsGenerated = true;
             }
             useButton.SetActive(_isItemConsumable);
@@ -80,21 +165,22 @@ namespace Equipment
             inventory.SetActive(isEnabled);
         }
 
-        private void OnItemClicked(int index)
+        private void OnItemClicked(Item item)
         {
             isItemDescriptionEnabled = true;
-            itemName.text = backpack[index].itemName;
-            itemDescriptionText.text = backpack[index].description;
-            icon.sprite = backpack[index].icon;
-            if (backpack[index] is Consumable)
+            itemName.text = item.itemName;
+            itemDescriptionText.text = item.description;
+            icon.sprite = item.icon;
+            if (item is Consumable consumable)
             {
                 _isItemConsumable = true;
-                useButton.GetComponent<Button>().onClick.AddListener(((Consumable)backpack[index]).Use);
+                useButton.GetComponent<Button>().onClick.AddListener(consumable.Use);
             }
             else
             {
                 _isItemConsumable = false;
             }
         }
+        
     }
 }
