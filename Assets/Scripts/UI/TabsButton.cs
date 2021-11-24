2637a30a54f912640bfb3cf12e55918bdf4b7845
CptAs <arpek2121@gmail.com>
2021-11-24 18:56:13 +0100

    Tabs system works

---
+++ Assets/Scripts/UI/TabsButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    [RequireComponent(typeof(Image))]
+    public class TabsButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+    {
+        public TabGroup tabGroup;
+
+        [NonSerialized]public Image background;
+        // Start is called before the first frame update
+        void Start()
+        {
+            background = GetComponent<Image>();
+            tabGroup.Subscribe(this);
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            tabGroup.OnTabEnter(this);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            tabGroup.OnTabSelected(this);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            tabGroup.OnTabExit(this);
+        }
+    }
+}
