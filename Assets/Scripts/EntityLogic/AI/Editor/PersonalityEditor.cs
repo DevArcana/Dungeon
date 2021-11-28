952e184ea91110b240ab3934fab92352c99c3e0e
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-11-28 20:39:57 +0100

    set dirty

--- Assets/Scripts/EntityLogic/AI/Editor/PersonalityEditor.cs
+++ Assets/Scripts/EntityLogic/AI/Editor/PersonalityEditor.cs
@@ -19,6 +19,8 @@ namespace EntityLogic.AI.Editor
             personality.maxTeamwork = EditorGUILayout.FloatField(personality.maxTeamwork);
             
             personality.isRanged = EditorGUILayout.ToggleLeft("Is Ranged", personality.isRanged);
+            
+            EditorUtility.SetDirty(personality);
         }
     }
 }
