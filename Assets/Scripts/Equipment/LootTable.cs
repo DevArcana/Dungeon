b02d80d011578a2e53ae0b35ebac6a5a4275c714
CptAs <arpek2121@gmail.com>
2021-12-12 00:39:01 +0100

    Added Loot tables

---
+++ Assets/Scripts/Equipment/LootTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Equipment
+{
+    [Serializable]
+    public class LootDrop
+    {
+        public Item item;
+        public int weight;
+    }
+    
+    [Serializable]
+    public class LootTable
+    {
+        public List<LootDrop> loot;
+        public int numberOfItemDropped;
+
+        public List<Item> GetDrop()
+        {
+            loot = loot.OrderBy(x => x.weight).ToList();
+            var returnedLoot = new List<Item>();
+            for (var i = 0; i < numberOfItemDropped; i++)
+            {
+                var roll = Random.Range(0, 101);
+                var weightedSum = 0;
+                foreach (var drop in loot)
+                {
+                    weightedSum += drop.weight;
+                    if (roll < weightedSum)
+                    {
+                        returnedLoot.Add(drop.item);
+                    }
+                }
+            }
+            return returnedLoot;
+        }
+    }
+}
